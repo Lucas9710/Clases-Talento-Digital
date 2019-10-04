22975b783e6737f81acc89c44e4deaa9fef9148e
Lucas9710 <38296412+Lucas9710@users.noreply.github.com>
2019-10-04 15:07:23 -0300

    Add files via upload
    Genera un archivo php que muestre una tabla con los registros de una base de datos

---
+++ tareasPractica4.php
@@ -0,0 +1,39 @@
+<?php
+
+$conexion = mysqli_connect("localhost", "root", "", "tareas");
+$sql = "SELECT* from tareas";
+$respuesta = mysqli_query($conexion, $sql);
+
+echo "<table>
+			<tr>
+			   <th>id</th>
+			   <th>tipo</th>
+			   <th>tarea</th>
+			   <th>estado</th>
+			</tr>"; 
+
+
+while($registro = mysqli_fetch_array($respuesta)){
+$id = $registro ["id"];
+$tipo = $registro ["tipo"];
+$tarea = $registro ["tarea"];
+$estado = $registro ["estado"];
+
+  echo "
+  		<tr>
+  		   <td> $id</td>
+  		   <td>$tipo</td>
+  		   <td>$tarea</td>
+  		   <td>$estado</td>
+  		</tr>"  ;
+
+  
+
+
+}
+
+echo "</table>";
+
+?>
+
+?>

71bcfe2efa750ce799e8ce5b2b7843317c0c3f08
Lucas9710 <38296412+Lucas9710@users.noreply.github.com>
2019-10-15 15:41:22 -0300

    ejercicio2 pdf7
    ejercicio2 pdf7

--- formularioLucas.php
+++ formularioLucas.php
@@ -1,37 +1,34 @@
 <?php
-$conexion = mysqli_connect("localhost:3307", "root", "", "todo");
+    
 
-if(!isset($_POST["realizada"])){
-	die();
+ $conexion = mysqli_connect("localhost:3307", "root", "", "tododb");
+$sql = "SELECT * FROM tareas";
+$respuesta= mysqli_query($conexion, $sql);
+echo "
+    <table>
+    <tr>
+      <th>id</th>
+      <th>tarea</th>
+      <th>realizada</th>
+      <th>fecha</th>
+    </tr>  
+";
+while($registro = mysqli_fetch_array($respuesta)){
+
+$id = $registro ["id"];
+$tarea = $registro ["tarea"];
+$realizada = $registro ["realizada"];
+$fecha = $registro ["fecha"];
+echo "    <th>$id</th>
+      <th>$tarea</th>
+      <th>$realizada</th>
+      <th>$fecha</th>
+      <td>
+ <a href='editarLucas.php?id=$id'>Editar</a>
+ </td>
+
+      
+    </tr>  ";
 }
-$fecha = DATE("d/m/y");
-$tareaIngresada = $_POST["tarea"];
-$largoDeTarea = strlen($tareaIngresada);
-
-if($largoDeTarea == 0){
- echo "<br> 
- no ingresaste nada
- </br";
-}
-//$realizada = $_POST["realizada"];
-
-if($_POST["realizada"] == "1"){
-	$realizada = "finalizada";
-}
-else {
-	$realizada = "";
-}
-$sql = "INSERT into tareas (estado,nombre,fecha)values ('$realizada', '$tareaIngresada', '$fecha') ";
-
-
-$respuesta = mysqli_query($conexion, $sql);
-
-if($respuesta == true && $largoDeTarea != 0 ){
-	echo "se realizo correctamente";
-}
-else {
-	echo "no se realizo correctamente";
-}
-
-
+echo "</table>";
 ?>

6ff5266392dc20e6d64923f043c2b81401866d21
Lucas9710 <38296412+Lucas9710@users.noreply.github.com>
2019-10-15 03:44:20 -0300

    Ejercicio1 PDF6

---
+++ filtro.php
@@ -0,0 +1,44 @@
+<?php
+
+    
+ $filtro = $_GET['tarea'];
+
+
+ $conexion = mysqli_connect("localhost", "root", "", "tododb");
+$sql = "SELECT* FROM tareas WHERE nombre LIKE '%$filtro%'";
+$respuesta= mysqli_query($conexion, $sql);
+
+
+if($respuesta == true){
+    echo "se logro la consulta";
+    
+}else{
+    echo "No se logro la consulta";
+}
+echo "
+    <table>
+    <tr>
+      <th>id</th>
+      <th>tarea</th>
+      <th>realizada</th>
+      <th>fecha</th>
+    </tr>  
+";
+while($registro = mysqli_fetch_array($respuesta)){
+$id = $registro ["id"];
+$tarea = $registro ["tarea"];
+$realizada = $registro ["realizada"];
+$fecha = $registro ["fecha"];
+
+echo "    <th>$id</th>
+      <th>$tarea</th>
+      <th>$realizada</th>
+      <th>$fecha</th>
+    </tr>  ";
+}
+
+echo "</table>";
+
+?>
+    
+    
